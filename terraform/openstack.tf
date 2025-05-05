a64dafb1741614883ac99b516ff5f742d9cce7a4
Stanislava <149074508+StanislavaGus@users.noreply.github.com>
2025-05-05 19:13:31 +0300

    Create openstack.tf
    add trf

---
+++ terraform/openstack.tf
@@ -0,0 +1,116 @@
+terraform {
+  required_version = ">= 1.11.2"
+
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "3.0.0"
+    }
+  }
+}
+
+provider "openstack" {
+  user_name   = var.os_user_name
+  password    = var.os_password
+  auth_url    = var.os_auth_url
+  tenant_name = var.os_tenant_name
+}
+
+# --- Получаем существующую сеть по имени ---
+data "openstack_networking_network_v2" "students_net" {
+  name = "sutdents-net"
+}
+
+# --- Берём стандартную security group по имени "default" ---
+data "openstack_networking_secgroup_v2" "default_sg" {
+  name = "default"
+}
+
+# --- Создаём свою security group и правило SSH ---
+resource "openstack_networking_secgroup_v2" "guseva_group" {
+  name        = "guseva-group"
+  description = "SSH access for guseva-instance"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "guseva_group_ssh" {
+  security_group_id = openstack_networking_secgroup_v2.guseva_group.id
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = "0.0.0.0/0"
+}
+
+# --- Создаём порт и прикрепляем к нему SG ---
+resource "openstack_networking_port_v2" "server_port" {
+  name       = "guseva-instance-port"
+  network_id = data.openstack_networking_network_v2.students_net.id
+
+  # вместо security_groups
+  security_group_ids = [
+    data.openstack_networking_secgroup_v2.default_sg.id,
+    openstack_networking_secgroup_v2.guseva_group.id,
+  ]
+}
+
+# --- Создаём инстанс, используя порт ---
+resource "openstack_compute_instance_v2" "guseva_server" {
+  name              = "guseva-instance-01-trf"
+  flavor_name       = "m1.small"
+  image_id          = "d608627a-ef62-452d-8a74-1c307cbe276d"  # ubuntu 22.04
+  availability_zone = "nova"
+  key_pair          = "guseva_deploy_key"
+
+  network {
+    port = openstack_networking_port_v2.server_port.id
+  }
+
+  # Чтобы не было гонки с SG, явно указываем зависимость
+  depends_on = [
+    openstack_networking_secgroup_v2.guseva_group,
+  ]
+}
+
+# --- Выводим IP для доступа и Ansible ---
+output "server_ip" {
+  description = "IPv4 address of guseva-instance-01"
+  value       = openstack_compute_instance_v2.guseva_server.access_ip_v4
+}
+
+# --- Ждём, пока SSH сервис станет доступным ---
+resource "null_resource" "wait_for_ssh" {
+  depends_on = [openstack_compute_instance_v2.guseva_server]
+
+  connection {
+    type             = "ssh"
+    user             = "ubuntu"
+    host             = openstack_compute_instance_v2.guseva_server.access_ip_v4
+
+    agent            = true              # используем ssh-agent
+    timeout          = "5m"
+  }
+
+  provisioner "remote-exec" {
+    inline = ["echo SSH is ready."]
+  }
+}
+
+# --- Генерим динамический inventory для Ansible ---
+resource "time_static" "now" {}
+
+resource "null_resource" "generate_inventory" {
+  depends_on = [null_resource.wait_for_ssh]
+
+  triggers = {
+    server_ip = openstack_compute_instance_v2.guseva_server.access_ip_v4
+    run_id    = time_static.now.id
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "[restaurantbot]" > hosts.ini
+      echo "${openstack_compute_instance_v2.guseva_server.access_ip_v4}" >> hosts.ini
+    EOT
+  }
+}
