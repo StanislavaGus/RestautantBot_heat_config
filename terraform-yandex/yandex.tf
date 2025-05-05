f38ffc62637d70d74b60fb0b2eb45643f5fd2726
Stanislava <149074508+StanislavaGus@users.noreply.github.com>
2025-05-05 23:34:58 +0300

    Create yandex.tf

---
+++ terraform-yandex/yandex.tf
@@ -0,0 +1,150 @@
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.7"
+    }
+  }
+}
+
+
+#
+# Провайдер
+#
+provider "yandex" {
+  token     = var.yandex_token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
+}
+
+#
+# Сеть и подсеть
+#
+data "yandex_vpc_network" "default" {
+  folder_id = var.folder_id
+  name      = "default"
+}
+
+resource "yandex_vpc_subnet" "guseva_subnet" {
+  name           = "guseva-subnet"
+  zone           = var.zone
+  network_id     = data.yandex_vpc_network.default.id
+  v4_cidr_blocks = ["192.168.30.0/24"]
+}
+
+
+#
+# Security group + правило SSH
+#
+resource "yandex_vpc_security_group" "guseva_sg" {
+  name       = "guseva-sg"
+  network_id = data.yandex_vpc_network.default.id
+}
+
+resource "yandex_vpc_security_group_rule" "guseva_allow_ssh" {
+  security_group_binding = yandex_vpc_security_group.guseva_sg.id
+  direction              = "ingress"
+  description            = "Allow SSH for guseva"
+  v4_cidr_blocks         = ["0.0.0.0/0"]
+  protocol               = "TCP"
+  port                   = 22
+}
+
+# HTTP Ingress
+resource "yandex_vpc_security_group_rule" "guseva_allow_http" {
+  security_group_binding = yandex_vpc_security_group.guseva_sg.id
+  direction              = "ingress"
+  description            = "Allow HTTP"
+  v4_cidr_blocks         = ["0.0.0.0/0"]
+  protocol               = "TCP"
+  port                   = 80
+}
+
+# HTTPS Ingress
+resource "yandex_vpc_security_group_rule" "guseva_allow_https" {
+  security_group_binding = yandex_vpc_security_group.guseva_sg.id
+  direction              = "ingress"
+  description            = "Allow HTTPS"
+  v4_cidr_blocks         = ["0.0.0.0/0"]
+  protocol               = "TCP"
+  port                   = 443
+}
+
+# All Egress (outbound)
+resource "yandex_vpc_security_group_rule" "guseva_allow_all_egress" {
+  security_group_binding = yandex_vpc_security_group.guseva_sg.id
+  direction              = "egress"
+  description            = "Allow all outbound traffic"
+  v4_cidr_blocks         = ["0.0.0.0/0"]
+  protocol               = "ANY"
+}
+
+#
+# Диск
+#
+resource "yandex_compute_disk" "guseva_boot_disk" {
+  name     = "guseva-boot-disk"
+  type     = "network-hdd"
+  zone     = var.zone
+  size     = 20
+  image_id = var.image_id
+}
+
+#
+# Виртуальная машина
+#
+resource "yandex_compute_instance" "guseva_vm" {
+  name = "guseva-vm"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.guseva_boot_disk.id
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.guseva_subnet.id
+    nat                = true
+    security_group_ids = [ yandex_vpc_security_group.guseva_sg.id ]
+  }
+
+  # Добавляем SSH-ключ для пользователя ubuntu
+  metadata = {
+    ssh-keys = "ubuntu:${chomp(file(var.ssh_public_key_path))}"
+  }
+}
+
+# Добавляем провайдер time для фиксации момента запуска
+resource "time_static" "now" {}
+
+# Генерация динамического inventory для Ansible
+resource "null_resource" "generate_inventory" {
+  # Зависим от создания ВМ и фиксации времени
+  depends_on = [
+    yandex_compute_instance.guseva_vm,
+    time_static.now,
+  ]
+
+  # Чтобы при каждом плане с новым time_static.now.id перегенерить файл
+  triggers = {
+    server_ip = yandex_compute_instance.guseva_vm.network_interface.0.nat_ip_address
+    run_id    = time_static.now.id
+  }
+
+  provisioner "local-exec" {
+    # команда, создающая hosts.ini
+    command = <<EOT
+      echo "[restaurantbot]" > hosts.ini
+      echo "${yandex_compute_instance.guseva_vm.network_interface.0.nat_ip_address}" >> hosts.ini
+    EOT
+  }
+}
